05cb3dee23c1c43fd79bd01bcabbb07db4a6a68b
Abdullah Mansoor <abdullahmansoor277@gmail.com>
2023-07-29 10:35:04 +0500

    fix: moved usequery to where it is needed in the product detail page

--- src/components/product-detail/product-detail-card.jsx
+++ src/components/product-detail/product-detail-card.jsx
@@ -3,9 +3,14 @@ import { Box, Button, Card, CircularProgress, Typography, styled } from '@mui/ma
 import { CenterAlignedContainer } from '../shared/center-aligned-container';
 import MessageContainer from '../shared/message-container';
 import { ArrowBack } from '@mui/icons-material';
-import { useNavigate } from 'react-router';
+import { useNavigate, useParams } from 'react-router';
 import { toSentenceCase } from '../../utils/toSentenceCase';
 import { formatPrice } from '../../utils/format-price';
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import axios from '../../config/axios';
+import { ENDPOINTS } from '../../constants/endpoints';
+import { useQuery } from '@tanstack/react-query';
 
 const specsKeyMap = {
   id: 'ID',
@@ -16,7 +21,7 @@ const specsKeyMap = {
   category: 'Category'
 };
 
-function ProductDetailCard(props) {
+function ProductDetailCard() {
   const navigate = useNavigate();
 
   return (
@@ -24,16 +29,34 @@ function ProductDetailCard(props) {
       <StyledButton onClick={() => navigate('/products')} variant="text">
         <ArrowBack /> Back
       </StyledButton>
-      <CardContent {...props} />
+      <CardContent />
     </StyledCard>
   );
 }
 
 export default ProductDetailCard;
 
-function CardContent(props) {
-  const { isLoading, error, detail } = props;
-  if (isLoading && !detail)
+function CardContent() {
+  const params = useParams();
+  const { products } = useSelector((state) => state.products);
+  const selectedProduct = products.find((p) => p.id === parseInt(params.id, 10));
+  const [productDetails, setProductDetails] = useState(null);
+
+  const fetchProduct = () => axios.get(`${ENDPOINTS.PRODUCT_DETAIL}/${params.id}`);
+
+  const { data, error, isLoading } = useQuery(['product-detail'], fetchProduct, {
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    enabled: !selectedProduct
+  });
+
+  useEffect(() => {
+    if (!selectedProduct && data?.data) {
+      setProductDetails(data?.data);
+    } else setProductDetails(selectedProduct);
+  }, [data?.data, selectedProduct?.id]);
+
+  if (isLoading && !productDetails)
     return (
       <CenterAlignedContainer>
         <CircularProgress />
@@ -47,15 +70,15 @@ function CardContent(props) {
       </CenterAlignedContainer>
     );
 
-  if (detail)
+  if (productDetails)
     return (
       <ProductDetailsContainer>
-        <ProductThumbnail src={detail.thumbnail}></ProductThumbnail>
+        <ProductThumbnail src={productDetails.thumbnail}></ProductThumbnail>
         <ProductTitleContainer>
-          <Typography variant="h5">{detail.title ?? ''}</Typography>
-          <Typography variant="body1">{detail.description}</Typography>
-          <Typography variant="h6">{formatPrice(detail.price ?? 0)}</Typography>
-          <Typography variant="body2">Discount: {detail.discountPercentage ?? 0}%</Typography>
+          <Typography variant="h5">{productDetails.title ?? ''}</Typography>
+          <Typography variant="body1">{productDetails.description}</Typography>
+          <Typography variant="h6">{formatPrice(productDetails.price ?? 0)}</Typography>
+          <Typography variant="body2">Discount: {productDetails.discountPercentage ?? 0}%</Typography>
         </ProductTitleContainer>
         <ProductSpecsContainer>
           <Typography mb={2} pb={1} borderBottom={'1px solid #eee'} variant="h6">
@@ -65,7 +88,7 @@ function CardContent(props) {
             {Object.keys(specsKeyMap).map((key) => (
               <Box className="list-item" key={key}>
                 <Typography fontWeight="500">{specsKeyMap[key]}:</Typography>
-                <Typography>{toSentenceCase(detail[key]?.toString())}</Typography>
+                <Typography>{toSentenceCase(productDetails[key]?.toString())}</Typography>
               </Box>
             ))}
           </Box>
@@ -75,8 +98,8 @@ function CardContent(props) {
             Images
           </Typography>
           <Box className="image-list">
-            {detail.images.map((image, idx) => (
-              <img key={`${detail.title}-${idx}`} className="image-item" src={image} alt={`${detail.title}-${idx}`} />
+            {productDetails.images.map((image, idx) => (
+              <img key={`${productDetails.title}-${idx}`} className="image-item" src={image} alt={`${productDetails.title}-${idx}`} />
             ))}
           </Box>
         </ProductImagesContainer>
